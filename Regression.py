510ad56662844ada43bdb9c627727dedacc1e56b
m.ezzeddine <mazen.ezzeddine@hps-worldwide.com>
2021-05-04 09:42:01 +0200

    Autoregression with online learning using RLS on a non linear sinusoidal function with trend.

---
+++ Regression.py
@@ -0,0 +1,72 @@
+
+
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+
+from myRLS import myRLS
+
+
+class Regression:
+
+    #def __init__(self):
+        # plt.style.use('seaborn')
+        # mpl.rcParams['font.family'] = 'serif'
+    def f(self, x):
+        return np.sin(x) + 0.5 * x
+
+    def create_plot(self, x, y, styles, labels, axlabels):
+        plt.figure(figsize=(10, 6))
+        for i in range(len(x)):
+            plt.plot(x[i], y[i], styles[i], label=labels[i])
+            plt.xlabel(axlabels[0])
+            plt.ylabel(axlabels[1])
+            plt.legend(loc=0)
+
+    def create(self):
+        x = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+        self.create_plot([x], [self.f(x)], ['b'], ['f(x)'], ['x', 'f(x)'])
+        res = np.polyfit(x, self.f(x), deg=1, full=True)
+        ry = np.polyval(res[0], x)
+        ry = np.polyval(res[0], x)
+        #return ry
+        self.create_plot([x, x], [self.f(x), ry], ['b', 'r.'], ['f(x)', 'regression'], ['x', 'f(x)'])
+        plt.show()
+
+
+    def createRLS(self):
+        x = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+
+        test_size = len(x)
+        lam = 0.95
+        num_vars = 2
+        LS = myRLS(num_vars, lam)
+        pred_x = []
+        pred_y = []
+        X = np.matrix(np.zeros((1, num_vars)))
+        pred_error = []
+        for t in range(test_size - num_vars):
+            for j in range(num_vars):
+                X[0, j] = self.f(x[t + j])
+            y1 = self.f(x[t + num_vars])  # predict
+            pred_x.append(t)
+            pred_y.append(float(X * LS.w))
+            print("index, prediction, output", t + num_vars + 1, float(X * LS.w), float(y1))
+            pred_error.append(LS.get_error())
+            LS.add_obs(X.T, y1)
+        ax = plt.plot(pred_x[0:], pred_y[0:], label='predicted')
+        ax = plt.plot(pred_x[0:], self.f(x[num_vars:]), label='actual')
+        _ = plt.plot(pred_x[0:], pred_error[0:], label='Error/residual')
+        # _ = plt.plot(x[num_vars:], self.create()[num_vars:], label='simple linear regression')
+        plt.legend()
+        plt.show()
+
+            # sum += abs(df[i] - pred_y[i])
+
+
+
+
+
+
+
